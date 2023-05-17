560cc7d2fbee84ff0c01e6a62fe497acccaa2386
Eldar <bef1eldar@gmail.com>
2023-05-17 23:54:32 +0300

    fix boolean completion  = false;

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        boolean completion  = true;
+        boolean completion  = false;
         if (completion) {
             new Generator().completion();
         }
